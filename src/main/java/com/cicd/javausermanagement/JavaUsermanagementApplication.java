321d18e3ed407c8c51110ea9ee73ca9df55f263f
Abhimanyu Garg <abhimanyu.garg@sap.com>
2023-09-22 10:55:39 -0700

    initial commit

---
+++ src/main/java/com/cicd/javausermanagement/JavaUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.javausermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUsermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/javausermanagement/JavaUsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.javausermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
